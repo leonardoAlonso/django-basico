5fcadbb146d8f1a10da114913f233529ebfba6dc
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-02-07 15:17:50 -0600

    add initial views and models, also adding it to admin

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    # polls/:id
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/vote/', views.vote, name="vote"),
+    path('<int:question_id>/results/', views.results, name="results")
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -5,4 +5,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('Estas en la pagina  principal')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero: {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero: {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero: {question_id}")
