e1a1ff3d49eca547af7f931a652bd47d865c73ef
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-02-07 16:43:15 -0600

    add templates

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Estas en la pagina  principal')
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {
+        'latest_question_list': latest_question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    print(question)
+    return render(request, 'polls/details.html', {
+        'question': question
+    })
 
 
 def results(request, question_id):
